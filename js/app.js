42528e2867e9eb8c1798c22df8dfbcce1690df65
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-23 17:32:37 +0100

    feat: Add Player with 3 methods

--- js/app.js
+++ js/app.js
@@ -27,7 +27,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function() {
 
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+    
+}
+
+Player.prototype.handleInput = function(input) {
+    
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
