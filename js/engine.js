38fcd608cc705c189742c7e81f3e93c9059f2222
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-24 15:24:56 +0100

    feat: Load all character images

--- js/engine.js
+++ js/engine.js
@@ -242,6 +242,10 @@ var Engine = (function(global) {
     'images/grass-block.png',
     'images/enemy-bug.png',
     'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png',
     'images/heart-small.png',
   ]);
   Resources.onReady(init);
