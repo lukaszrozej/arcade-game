671f52f7b7278e429e11dd7f71183d3ec33ab3e4
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-24 13:38:08 +0100

    refactor: Add event listener in engine

--- js/app.js
+++ js/app.js
@@ -115,16 +115,3 @@ allEnemies = [
 ];
 
 player = new Player();
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-  };
-
-  player.handleInput(allowedKeys[e.keyCode]);
-});
--- js/engine.js
+++ js/engine.js
@@ -68,8 +68,24 @@ var Engine = (function(global) {
    */
   function init() {
     reset();
+    addEventListeners();
     lastTime = Date.now();
     main();
+  }
+
+  function addEventListeners() {
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keyup', function(e) {
+      var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+      };
+
+      player.handleInput(allowedKeys[e.keyCode]);
+    });
   }
 
   // Checks if any of the enemies collides with the player
