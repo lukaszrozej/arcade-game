d915d1a7a80413c72b5a7f51738a9f7fbb482894
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-27 07:42:36 +0200

    feat: Add levels.js

---
+++ js/levels.js
@@ -0,0 +1,45 @@
+const levels = [{
+    rowImages: [
+      'images/water-block.png',
+      'images/stone-block.png',
+      'images/stone-block.png',
+      'images/stone-block.png',
+      'images/grass-block.png',
+      'images/grass-block.png'
+    ],
+    enemyOptions: {
+      rows: [
+        { row: 1, direction: 1 },
+        { row: 2, direction: -1 },
+        { row: 3, direction: 1 },
+      ],
+      maxSpeed: 300,
+      minSpeed: 100
+    },
+    numberOfEnemies: 3,
+    message: `I'd better avoid
+              these bugs.`
+  },
+  {
+    rowImages: [
+      'images/water-block.png',
+      'images/stone-block.png',
+      'images/stone-block.png',
+      'images/stone-block.png',
+      'images/grass-block.png',
+      'images/grass-block.png'
+    ],
+    enemyOptions: {
+      rows: [
+        { row: 1, direction: 1 },
+        { row: 2, direction: -1 },
+        { row: 3, direction: 1 },
+      ],
+      maxSpeed: 300,
+      minSpeed: 100
+    },
+    numberOfEnemies: 3
+    message: `I'd better avoid
+              these bugs.`
+  },
+]
