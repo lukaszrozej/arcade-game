b08fd2b39bbfb487f9d51dbdbbd86f64ed28fe19
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-04-15 07:12:57 +0200

    feat: Add explode()

--- js/player.js
+++ js/player.js
@@ -150,6 +150,11 @@ class Player {
     this.animation.initialize();
   }
 
+  explode() {
+    this.animation = this.explodeAnimation;
+    this.animation.initialize();
+  }
+
   currentPosition() {
     return {
       x: this.col * 101,
