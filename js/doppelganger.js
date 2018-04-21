b971ac03b8c92552d6e07e919b50db0c8ce80def
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-04-21 11:54:04 +0200

    docs: Add Doppelganger comments

--- js/doppelganger.js
+++ js/doppelganger.js
@@ -1,5 +1,15 @@
+/** Class representing player's doppelganger
+ */
 class Doppelganger extends Player{
-  constructor(sprite = 'images/char-horn-girl.png', startingPosition) {
+
+  /** Create doppelganger
+   * @param {string} sprite - url of the image of the player sprite
+   *  The image should contain 3 sections:
+   *    - top section for the whole body
+   *    - middle section for the head
+   *    - bottom section for the trunk
+   */
+  constructor(sprite = 'images/char-horn-girl.png') {
     super(sprite, { y: 0 });
 
     this.body.filter = 'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)';
